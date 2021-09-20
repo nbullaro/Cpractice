2e877c005e522bc63b5e7ada15f0b3eb205834e9
nbullaro <54286581+nbullaro@users.noreply.github.com>
2021-09-20 16:36:15 -0500

    Add files via upload

---
+++ imp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	//float x = 50.0;
+	int slices = 17;
+	int people = 2;
+	double halfslice =(double) slices /people;
+
+
+	printf("%f\n",halfslice);	
+	
+	double c = 25/2 * 2;
+	double d =(double) 25/2 * 2.0;
+
+	printf("%f\n", d);
+
+	return 0;
+}
---
+++ inca.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int pizza = 100;
+
+	
+
+	printf("Pizzas to eat: %i\n",pizza);
+	pizza += 100;
+	pizza -= 100;
+	pizza *= 2;
+	pizza /= 4;
+	pizza %= 5;
+	printf("%i\n",pizza);
+	return 0;
+}
